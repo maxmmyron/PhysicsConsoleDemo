78040bbf5e8d14e75cbaf912c3c8ff589c3fd4bc
maxmmyron <maxmmyron@gmail.com>
2021-01-10 14:34:57 -0500

    preparation

--- PhysicsConsoleDemo/main.cpp
+++ PhysicsConsoleDemo/main.cpp
@@ -1,9 +1,11 @@
 #include <Engine.h>
 #include <Particle.h>
 
+#include <Polygon.h>
+
 int main()
 {
-	Engine e = Engine();
+	/*Engine e = Engine();
 
 	Particle::MovementData md1 = { Vector(10,0), Vector(1, 0), Vector() };
 	Particle::MovementData md2 = { Vector(12,0), Vector(-1, 0), Vector() };
@@ -22,5 +24,9 @@ int main()
 	std::cout << e.GetEngineTimeAsDouble() << std::endl;
 
 	p1.DebugPrint();
-	p2.DebugPrint();
+	p2.DebugPrint();*/
+
+	GameObject::MovementData md1 = { Vector(10,0), Vector(1, 0), Vector() };
+	PhysicsEngine::Polygon::PolygonData pd1 = { 5, {Vector(3,5), Vector(6,3), Vector(4,1), Vector(2,2), Vector(1,4)}, {1} };
+	PhysicsEngine::Polygon p = PhysicsEngine::Polygon("Polygon A", 1.0f, md1, pd1);
 }
