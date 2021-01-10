707050b7f10f8eaad8a4769e2099d1b9b3972feb
maxmmyron <maxmmyron@gmail.com>
2021-01-10 07:37:48 -0500

    refactor

--- PhysicsConsoleDemo/main.cpp
+++ PhysicsConsoleDemo/main.cpp
@@ -1,26 +1,21 @@
-// PhysicsConsoleDemo.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
-#include <Particle.h>
-#include <Vector.h>
 #include <Engine.h>
-
-#include <type_traits>
-#include <iterator>
-
-#include<File.h>
-
-Engine e = Engine();
 
 int main()
 {
-	Particle p1 = Particle("Particle A", 3.0f, Vector(), Vector(1, 0), Vector());
-	Particle p2 = Particle("Particle B", 3.0f, Vector(), Vector(), Vector(1, 0));
+	Engine e = Engine();
+
+	Particle::MovementData md1 = { Vector(10,0), Vector(1, 0), Vector() };
+	Particle::MovementData md2 = { Vector(12,0), Vector(-1, 0), Vector() };
+
+	Particle::ParticleArguments pa1 = { 1.0f, {1} };
+	Particle::ParticleArguments pa2 = { 1.0f, {1} };
+
+	Particle p1 = Particle("Particle A", 1.0f, md1, pa1);
+	Particle p2 = Particle("Particle B", 1.0f, md2, pa2);
 
 	e.AddParticle(&p1);
 	e.AddParticle(&p2);
-	
+
 	e.StartLoop();
 
 	std::cout << e.GetEngineTimeAsDouble() << std::endl;
