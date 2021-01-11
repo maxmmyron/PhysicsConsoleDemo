13a9fcd5ca781fdebf6fbf2e54c0c20c3bf2435b
maxmmyron <maxmmyron@gmail.com>
2021-01-11 17:39:42 -0500

    minor changes

--- PhysicsConsoleDemo/main.cpp
+++ PhysicsConsoleDemo/main.cpp
@@ -7,7 +7,7 @@ using namespace PhysicsEngine;
 
 int main()
 {
-	Engine e = Engine();
+	/*Engine e = Engine();
 
 	Particle::MovementData md1 = { Vector(10,0), Vector(1, 0), Vector() };
 	Particle::MovementData md2 = { Vector(12,0), Vector(-1, 0), Vector() };
@@ -26,5 +26,17 @@ int main()
 	std::cout << e.GetEngineTimeAsDouble() << std::endl;
 
 	p1.DebugPrint();
-	p2.DebugPrint();
+	p2.DebugPrint();*/
+
+	Particle::MovementData md1 = { 0, 0, 0 };
+
+	Polygon::PolygonData pd1 = { 3, {Vector(1,1), Vector(1,2), Vector(2,2)}, {1} };
+	Polygon::PolygonData pd2 = { 3, {Vector(3,3), Vector(3,4), Vector(4,4)}, {1} };
+
+	PhysicsEngine::Polygon a("a", 1, md1, pd1);
+	PhysicsEngine::Polygon b("a", 1, md1, pd2);
+
+
+	std::cout << a.SATCollision(b) << std::endl;
+	std::cout << b.SATCollision(a) << std::endl;
 }
